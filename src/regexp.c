542a4ddd17cda1f9aac6369c47dc0f5a51c32d19
Lai-YT <381xvmvbib@gmail.com>
2023-03-11 17:49:24 +0800

    refactor: add comment & rename variable & extract function

--- src/regexp.c
+++ src/regexp.c
@@ -1,69 +1,143 @@
+#include <stdbool.h>
 #include <string.h>
 
 #include "regex.h"
 
-char* re2post(const char* re) {
-  int nalt, natom;
-  static char buf[8000];
-  char* dst;
-  struct {
-    int nalt;
-    int natom;
-  } paren[100], *p;
+#define BUF_SIZE 8000
+#define MAX_NESTED_DEPTH 100
+#define EXPLICIT_CONCAT '.'
 
-  p = paren;
-  dst = buf;
-  nalt = 0;
-  natom = 0;
-  if (strlen(re) >= sizeof buf / 2) return NULL;
+typedef struct {
+  /* union has the lowest precedence, record separately */
+  int num_of_union;
+  int num_of_unit;
+} unit_t;
+
+/**
+ * @brief whether the buffer may overflow after adding explicit concatenation
+ * symbols.
+ */
+static bool buf_may_overflow(const char* re);
+static void stash_unit(unit_t** unit_stack, unit_t unit);
+static void restore_unit(unit_t** unit_stack, unit_t* unit);
+static void init_unit(unit_t* unit);
+
+char* re2post(const char* re) {
+  static char buf[BUF_SIZE];
+  char* buf_top = buf;
+
+  /*
+   * We are to track the parentheses with a stack, and counts the number of
+   * operation units so we know where to place an operator after every two
+   * units. An operation units can be a single symbol or a parenthesized set of
+   * symbols/operators. Each parenthesized set of symbols/operators is treated
+   * as a single unit after being transformed.
+   */
+
+  // paren_units works as a stack. Stashing the nested parentheses units seen
+  // so far, so we can restore them after transforming inner nested
+  // parenthesized units. Treat the transformed unit as a single unit, and
+  // resume the transformation.
+  unit_t paren_units[MAX_NESTED_DEPTH];
+  unit_t* top_unit = paren_units;  // its in fact the one above the top
+  unit_t curr_paren_unit = {.num_of_union = 0, .num_of_unit = 0};
+
+  if (buf_may_overflow(re)) {
+    return NULL;
+  }
   for (; *re; re++) {
     switch (*re) {
       case '(':
-        if (natom > 1) {
-          --natom;
-          *dst++ = '.';
+        // Starts a new parenthesized operation unit. It's concatenated with the
+        // operation unit on its left, so add a concat symbol.
+        if (curr_paren_unit.num_of_unit > 1) {
+          --curr_paren_unit.num_of_unit;
+          *buf_top++ = EXPLICIT_CONCAT;
         }
-        if (p >= paren + 100) return NULL;
-        p->nalt = nalt;
-        p->natom = natom;
-        p++;
-        nalt = 0;
-        natom = 0;
+        // error, too many nested parentheses, stack overflows
+        if (top_unit - paren_units > MAX_NESTED_DEPTH) {
+          return NULL;
+        }
+        stash_unit(&top_unit, curr_paren_unit);
+        init_unit(&curr_paren_unit);
         break;
       case '|':
-        if (natom == 0) return NULL;
-        while (--natom > 0) *dst++ = '.';
-        nalt++;
+        // error, no left unit to union with
+        if (curr_paren_unit.num_of_unit == 0) {
+          return NULL;
+        }
+        while (--curr_paren_unit.num_of_unit > 0) {
+          *buf_top++ = EXPLICIT_CONCAT;
+        }
+        curr_paren_unit.num_of_union++;
         break;
       case ')':
-        if (p == paren) return NULL;
-        if (natom == 0) return NULL;
-        while (--natom > 0) *dst++ = '.';
-        for (; nalt > 0; nalt--) *dst++ = '|';
-        --p;
-        nalt = p->nalt;
-        natom = p->natom;
-        natom++;
+        // error, nothing inside the parenthesis
+        if (top_unit == paren_units || curr_paren_unit.num_of_unit == 0) {
+          return NULL;
+        }
+        while (--curr_paren_unit.num_of_unit > 0) {
+          *buf_top++ = EXPLICIT_CONCAT;
+        }
+        while (curr_paren_unit.num_of_union-- > 0) {
+          *buf_top++ = '|';
+        }
+        // the nested parenthesized unit is transformed, restore
+        restore_unit(&top_unit, &curr_paren_unit);
+        curr_paren_unit.num_of_unit++;
         break;
       case '*':
       case '+':
       case '?':
-        if (natom == 0) return NULL;
-        *dst++ = *re;
+        // error, nothing to operate on
+        if (curr_paren_unit.num_of_unit == 0) {
+          return NULL;
+        }
+        // they have the highest precedence
+        *buf_top++ = *re;
         break;
       default:
-        if (natom > 1) {
-          --natom;
-          *dst++ = '.';
+        if (curr_paren_unit.num_of_unit > 1) {
+          --curr_paren_unit.num_of_unit;
+          *buf_top++ = EXPLICIT_CONCAT;
         }
-        *dst++ = *re;
-        natom++;
+        *buf_top++ = *re;
+        curr_paren_unit.num_of_unit++;
         break;
     }
   }
-  if (p != paren) return NULL;
-  while (--natom > 0) *dst++ = '.';
-  for (; nalt > 0; nalt--) *dst++ = '|';
-  *dst = 0;
+
+  // unmatched parentheses
+  if (top_unit != paren_units) {
+    return NULL;
+  }
+  while (--curr_paren_unit.num_of_unit > 0) {
+    *buf_top++ = EXPLICIT_CONCAT;
+  }
+  while (curr_paren_unit.num_of_union-- > 0) {
+    *buf_top++ = '|';
+  }
+  *buf_top = '\0';
   return buf;
+}
+
+static bool buf_may_overflow(const char* re) {
+  // the number of concat symbol can at most be strlen(re) - 1 when re contains
+  // only concatenations
+  return strlen(re) > BUF_SIZE / 2;
+}
+
+static void stash_unit(unit_t** unit_stack, unit_t unit) {
+  **unit_stack = unit;
+  (*unit_stack)++;  // push
+}
+
+static void init_unit(unit_t* unit) {
+  *unit = (unit_t){.num_of_union = 0, .num_of_unit = 0};
+}
+
+static void restore_unit(unit_t** unit_stack, unit_t* unit) {
+  unit_t* top = *unit_stack - 1;
+  *unit = *top;
+  (*unit_stack)--;  // pop
 }
