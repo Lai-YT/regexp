f229619b366f84257b7d7cf92cbeb18c29f61a69
Lai-YT <381xvmvbib@gmail.com>
2023-03-12 21:12:25 +0800

    test: inline variables
    Simple test, eliminate boilerplate code.

--- test/main.c
+++ test/main.c
@@ -13,72 +13,37 @@
 
 static void test_re2post_concat(void** state) {
   (void)state;
-  const char* re = "abba";
-  const char* post = "ab.b.a.";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("abba"), "ab.b.a.");
 }
 
 static void test_re2post_union(void** state) {
   (void)state;
-  const char* re = "a|b|c|d|e|f";
-  const char* post = "abcdef|||||";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("a|b|c|d|e|f"), "abcdef|||||");
 }
 
 static void test_re2post_union_and_concat(void** state) {
   (void)state;
-  const char* re = "ab|ba";
-  const char* post = "ab.ba.|";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("ab|ba"), "ab.ba.|");
 }
 
 static void test_re2post_paren(void** state) {
   (void)state;
-  const char* re = "(a(b(c(d(e)))))";
-  const char* post = "abcde....";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("(a(b(c(d(e)))))"), "abcde....");
 }
 
 static void test_re2post_concatenation_with_paren(void** state) {
   (void)state;
-  const char* re = "a(bb)a";
-  const char* post = "abb..a.";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("a(bb)a"), "abb..a.");
 }
 
 static void test_re2post_union_with_paren(void** state) {
   (void)state;
-  const char* re = "a|((b|c)|(d|e))|f";
-  const char* post = "abc|de||f||";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("a|((b|c)|(d|e))|f"), "abc|de||f||");
 }
 
 static void test_re2post_mix(void** state) {
   (void)state;
-  const char* re = "a(bbbb|a|bab)+a";
-  const char* post = "abb.b.b.aba.b.||+.a.";
-
-  const char* result = re2post(re);
-
-  assert_string_equal(result, post);
+  assert_string_equal(re2post("a(bbbb|a|bab)+a"), "abb.b.b.aba.b.||+.a.");
 }
 
 int main(void) {
