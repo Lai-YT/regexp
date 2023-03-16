91208ca6f83ae3f722065995c0e804edc464989f
Lai-YT <381xvmvbib@gmail.com>
2023-03-16 16:19:24 +0800

    test: move test case definitions into header

--- test/main.c
+++ test/main.c
@@ -1,4 +1,4 @@
-#include "../src/re2post.h"
+#include "re2post.h"
 
 #include <setjmp.h>
 #include <stdarg.h>
@@ -10,41 +10,6 @@
 // library functions and types by having us include the necessary headers.
 #include <cmocka.h>
 // clang-format on
-
-static void test_re2post_concat(void** state) {
-  (void)state;
-  assert_string_equal(re2post("abba"), "ab.b.a.");
-}
-
-static void test_re2post_union(void** state) {
-  (void)state;
-  assert_string_equal(re2post("a|b|c|d|e|f"), "abcdef|||||");
-}
-
-static void test_re2post_union_and_concat(void** state) {
-  (void)state;
-  assert_string_equal(re2post("ab|ba"), "ab.ba.|");
-}
-
-static void test_re2post_paren(void** state) {
-  (void)state;
-  assert_string_equal(re2post("(a(b(c(d(e)))))"), "abcde....");
-}
-
-static void test_re2post_concatenation_with_paren(void** state) {
-  (void)state;
-  assert_string_equal(re2post("a(bb)a"), "abb..a.");
-}
-
-static void test_re2post_union_with_paren(void** state) {
-  (void)state;
-  assert_string_equal(re2post("a|((b|c)|(d|e))|f"), "abc|de||f||");
-}
-
-static void test_re2post_mix(void** state) {
-  (void)state;
-  assert_string_equal(re2post("a(bbbb|a|bab)+a"), "abb.b.b.aba.b.||+.a.");
-}
 
 int main(void) {
   const struct CMUnitTest tests[] = {
---
+++ test/re2post.h
@@ -0,0 +1,47 @@
+#include "../src/re2post.h"
+
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// clang-format off
+// cmocka allows test applications to use custom definitions of C standard
+// library functions and types by having us include the necessary headers.
+#include <cmocka.h>
+// clang-format on
+
+static void test_re2post_concat(void** state) {
+  (void)state;
+  assert_string_equal(re2post("abba"), "ab.b.a.");
+}
+
+static void test_re2post_union(void** state) {
+  (void)state;
+  assert_string_equal(re2post("a|b|c|d|e|f"), "abcdef|||||");
+}
+
+static void test_re2post_union_and_concat(void** state) {
+  (void)state;
+  assert_string_equal(re2post("ab|ba"), "ab.ba.|");
+}
+
+static void test_re2post_paren(void** state) {
+  (void)state;
+  assert_string_equal(re2post("(a(b(c(d(e)))))"), "abcde....");
+}
+
+static void test_re2post_concatenation_with_paren(void** state) {
+  (void)state;
+  assert_string_equal(re2post("a(bb)a"), "abb..a.");
+}
+
+static void test_re2post_union_with_paren(void** state) {
+  (void)state;
+  assert_string_equal(re2post("a|((b|c)|(d|e))|f"), "abc|de||f||");
+}
+
+static void test_re2post_mix(void** state) {
+  (void)state;
+  assert_string_equal(re2post("a(bbbb|a|bab)+a"), "abb.b.b.aba.b.||+.a.");
+}
