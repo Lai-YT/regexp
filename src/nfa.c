816430dcf802a3e3ecda0dae675160192b52ed5e
Lai-YT <381xvmvbib@gmail.com>
2023-03-28 09:43:07 +0800

    refactor: delete NFA with map

--- src/nfa.c
+++ src/nfa.c
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 
-#include "set.h"
+#include "map.h"
 #include "state.h"
 
 Nfa* create_nfa(State* start, State* accept) {
@@ -13,15 +13,15 @@ Nfa* create_nfa(State* start, State* accept) {
   return n;
 }
 
-/// @brief Collects all of the states reachable from start into the set.
-/// @param states Set of states which the states will be inserted into.
+/// @brief Collects all of the states reachable from start into the map.
+/// @param states Mapping of states which the states will be inserted into.
 /// @param start The start state which all the states reachable from it will be
 /// collected.
-static void collect_states(Set* states, State* start) {
-  if (has_key(states, start)) {
+static void collect_states(Map* states, State* start) {
+  if (get_value(states, start->id)) {
     return;
   }
-  insert_key(states, start);
+  insert_pair(states, start->id, start);
   if (start->label != ACCEPT) {
     collect_states(states, start->outs[0]);
     if (start->label == SPLIT) {
@@ -39,16 +39,16 @@ void delete_nfa(Nfa* nfa) {
 }
 
 /// @brief Deletes all of the states that are reachable from state and are in
-/// the set.
+/// the map.
 /// @note A helper function for delete_state_chain.
-static void delete_state_chain_recursion(Set* states_to_delete, State* state) {
-  if (!has_key(states_to_delete, state)) {
+static void delete_state_chain_recursion(Map* states_to_delete, State* state) {
+  if (!get_value(states_to_delete, state->id)) {
     return;
   }
   // Note that you have to mark as deleted before dealing with the outs,
   // otherwise if there's a cycle, you'll be in an infinite loop, which causes
   // the stack to overflow.
-  delete_key(states_to_delete, state);
+  delete_pair(states_to_delete, state->id);
   if (state->label != ACCEPT) {
     delete_state_chain_recursion(states_to_delete, state->outs[0]);
     if (state->label == SPLIT) {
@@ -59,8 +59,8 @@ static void delete_state_chain_recursion(Set* states_to_delete, State* state) {
 }
 
 static void delete_state_chain(State* s) {
-  Set* states_to_delete = create_set();
+  Map* states_to_delete = create_map();
   collect_states(states_to_delete, s);
   delete_state_chain_recursion(states_to_delete, s);
-  delete_set(states_to_delete);
+  delete_map(states_to_delete);
 }
