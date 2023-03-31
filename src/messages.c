322ab4454ece271eb36e9a560cf5e6dd67d3b0a6
Lai-YT <381xvmvbib@gmail.com>
2023-03-31 11:11:44 +0800

    docs: update help message

--- src/messages.c
+++ src/messages.c
@@ -24,7 +24,7 @@
  * Help message
  */
 void help() {
-  fprintf(stdout, BLUE __PROGRAM_NAME__ "\n\n" NO_COLOR);
+  fprintf(stdout, CYAN __PROGRAM_NAME__ "\n\n" NO_COLOR);
   usage();
   description();
   options();
@@ -37,7 +37,8 @@ void help() {
  */
 void usage() {
   fprintf(stdout, YELLOW "Usage: " NO_COLOR);
-  fprintf(stdout, "%s [options] [regexp string]\n\n", __PROGRAM_NAME__);
+  fprintf(stdout, "%s [-h] [-V] [-d regexp [-o FILE]] [regexp string]\n\n",
+          __PROGRAM_NAME__);
 }
 
 /*
@@ -50,20 +51,52 @@ void description() {
           "Supports only ( | ) * + ?. No escapes.\n"
           "Compiles to NFA and then simulates NFA using Thompson's algorithm.\n"
           "\n"
-          "Matches the string (string) to the regular expression (regexp) and "
-          "exits with 1 if it does not match.\n\n");
+          "One can either graph the regexp or match a string.\n"
+          "See the following options.\n"
+          "\n");
+}
+
+void graph_mode() {
+  fprintf(stdout, WHITE
+          "Graph mode:\n"
+          "  Converts the regular expression into a graph,\n"
+          "  exits with 1 if regexp is ill-formed or the file can't be opened\n"
+          "\n"
+          "  -d, --graph             Converts the NFA of the regexp into a "
+          "Graphviz\n"
+          "                        dot file (default: False)\n"
+          "  -o FILE, --output FILE\n"
+          "                        The name of the dot file.\n"
+          "                        A .dot extension is appended automatically\n"
+          "                        (default: nfa)\n"
+          "  regexp                The regular expression to be converted\n"
+          "\n" NO_COLOR);
+}
+
+void match_mode() {
+  fprintf(stdout, WHITE
+          "Match mode:\n"
+          "  Matches the string with the regular expression,\n"
+          "  exits with 1 if regexp is ill-formed or it does not match\n"
+          "\n"
+          "  regexp                The regular expression to use on matching\n"
+          "  string                The string to be matched\n"
+          "\n" NO_COLOR);
 }
 
 /*
  * Options message
  */
 void options() {
-  fprintf(stdout, YELLOW "Options:\n\n" NO_COLOR);
   fprintf(stdout,
-          WHITE "\t-v, --version\n" NO_COLOR "\t\tPrints %s version\n\n",
+          WHITE
+          "Options:\n"
+          "  -h, --help            Shows this help message and exit\n"
+          "  -V, --version         Shows %s version and exit\n"
+          "\n" NO_COLOR,
           __PROGRAM_NAME__);
-  fprintf(stdout,
-          WHITE "\t-h, --help\n" NO_COLOR "\t\tPrints this help message\n\n");
+  graph_mode();
+  match_mode();
 }
 
 /*
