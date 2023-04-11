2cdb63acf1c3c96656d59d51c52f4968fd1c22b2
Lai-YT <381xvmvbib@gmail.com>
2023-04-11 23:27:49 +0800

    test: precedence of special symbols

--- test/re2post.h
+++ test/re2post.h
@@ -23,6 +23,22 @@ static void test_re2post_union() {
   assert_string_equal(re2post("a|b|c|d|e|f"), "abcdef|||||");
 }
 
+static void test_re2post_zero_or_more() {
+  assert_string_equal(re2post("a*b"), "a*b#");
+}
+
+static void test_re2post_one_or_more() {
+  assert_string_equal(re2post("a+b"), "a+b#");
+}
+
+static void test_re2post_zero_or_one() {
+  assert_string_equal(re2post("a?b"), "a?b#");
+}
+
+static void test_re2post_any() {
+  assert_string_equal(re2post("a.b"), "a.#b#");
+}
+
 static void test_re2post_union_and_concat() {
   assert_string_equal(re2post("ab|ba"), "ab#ba#|");
 }
@@ -40,7 +56,8 @@ static void test_re2post_union_with_paren() {
 }
 
 static void test_re2post_mix() {
-  assert_string_equal(re2post("a(bbbb|a|bab)+a"), "abb#b#b#aba#b#||+#a#");
+  assert_string_equal(re2post("a(bb?b.b|a|b*ab)+a"),
+                      "abb?#b#.#b#ab*a#b#||+#a#");
 }
 
 static void test_re2post_empty_re_should_be_empty_post() {
